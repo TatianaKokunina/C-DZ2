5bae55cae8303dc57fb8d29aca17d60900a5a69d
TatianaKokunina <tanechka.91@mail.ru>
2022-11-24 08:41:42 +0300

    EX10

---
+++ EX10/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Write("Введите трехзначное число=");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int NewNumberSearch(int Number)
+{
+   int serNumber = (Number / 10) % 10;
+   return serNumber;
+}
+
+int serNumber = NewNumberSearch(Number);
+Console.WriteLine($"У числа {Number} вторая цифра= {serNumber}");
